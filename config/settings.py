977f0de3abc87ed232e01e1f3ec33c2bb2a5a9f2
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-07-07 07:34:28 +0300

    migrate

--- config/settings.py
+++ config/settings.py
@@ -78,18 +78,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-ATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postqresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME_DB'),
+        'USER': os.getenv('USER_DB'),
+        'PASSWORD': os.getenv('PASSWORD_DB'),
+        'HOST': os.getenv('HOST_DB'),
+        'PORT': os.getenv('PORT_DB'),
 
     }
 }
 
+print("DEBUG USER:", os.getenv("POSTGRES_USER"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
