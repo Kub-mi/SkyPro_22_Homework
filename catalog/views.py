90ef17914a6670a641a342a26620f0d7e82fd430
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-07-20 11:23:00 +0300

    task 2

--- catalog/views.py
+++ catalog/views.py
@@ -4,15 +4,13 @@ from catalog.models import Product
 
 
 def home_view(request):
-    latest_products = Product.objects.order_by('-created_at')[:5]
-    print("🔍 Последние 5 продуктов:")
-    for product in latest_products:
-        print(f"{product.name} — {product.created_at}")
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
 
-    return render(request, 'catalog/home.html', {'products': latest_products})
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
 
 def form_contacts(request):
     if request.method == 'POST':
@@ -24,6 +22,7 @@ def form_contacts(request):
 
     return render(request, 'catalog/contacts.html')
 
-def product_details(request, prod_id):
-    product = get_object_or_404(Product, id=prod_id)
+
+def product_details(request, id):
+    product = get_object_or_404(Product, id=id)
     return render(request,'catalog/product_details.html', {'product': product})
