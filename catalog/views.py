e0ce60d8c0ff8ae8a4f80a4ed883442efde856e6
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-08-09 15:03:48 +0300

    подготовка, добавил новые файлы

--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,9 @@
-from unicodedata import category
-
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from catalog.models import Product, Category
 from django.views.generic import ListView, DetailView, TemplateView, View, CreateView
 
-
-# def home_view(request):
-#     products = Product.objects.all()
-#     paginator = Paginator(products, 6)
-#
-#     page_num = request.GET.get('page')
-#     page_obj = paginator.get_page(page_num)
-#     return render(request, 'catalog/home.html', {'page_obj': page_obj})
 
 class HomeView(ListView):
     model = Product
@@ -22,23 +12,8 @@ class HomeView(ListView):
     paginate_by = 6
 
 
-# def contacts(request):
-#     return render(request, 'catalog/contacts.html')
-
-
 class ContactView(TemplateView):
     template_name = 'catalog/contacts.html'
-
-
-# def form_contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#
-#         return HttpResponse(f'Спасибо, {name}! Данные получены')
-#
-#     return render(request, 'catalog/contacts.html')
 
 
 class FormContactsView(View):
@@ -51,39 +26,12 @@ class FormContactsView(View):
         message = request.POST.get('message')
         return HttpResponse(f'Спасибо, {name}! Данные получены')
 
-# def product_details(request, id):
-#     product = get_object_or_404(Product, id=id)
-#     return render(request,'catalog/product_details.html', {'product': product})
-
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_details.html'
     context_object_name = 'product'
     pk_url_kwarg = 'id'
-
-
-# def add_product(request):
-#     if request.method == "POST":
-#         name = request.POST.get('name')
-#         description = request.POST.get('description')
-#         image = request.FILES.get('image')
-#         category_id = request.POST.get('category')
-#         price = request.POST.get('price')
-#
-#         category = Category.objects.get(pk=category_id)
-#
-#         Product.objects.create(
-#             name=name,
-#             description=description,
-#             image=image,
-#             category=category,
-#             price=price
-#         )
-#
-#         return redirect('/catalog/')
-#     categories = Category.objects.all()
-#     return render(request,'catalog/add_product.html', {'categories': categories})
 
 
 class AddProductView(CreateView):
