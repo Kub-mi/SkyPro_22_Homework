22fec5cf96af4f1148470b8f28c3cfcd5bb1d46d
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-08-24 15:00:38 +0300

    добавил в модель продукта доступ по публикации

--- catalog/models.py
+++ catalog/models.py
@@ -22,10 +22,15 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    is_published = models.BooleanField(default=False)
+
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        permissions =[
+            ('can_unpublish_product', 'Can unpublish product'),
+        ]
 
     def __str__(self):
         return self.name
